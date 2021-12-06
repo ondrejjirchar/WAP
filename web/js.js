b44cab3e1fd4e0a167a21462692cc6673b30806c
ondrejjirchar <92851722+ondrejjirchar@users.noreply.github.com>
2021-12-06 23:19:54 +0100

    Add files via upload

--- web/js.js
+++ web/js.js
@@ -1,4 +1,4 @@
-let data = [];
+/*let data = [];
 let form = document.getElementById("form");
 form.onsubmit = (e) => {
     e.preventDefault();
@@ -9,25 +9,54 @@ form.onsubmit = (e) => {
     
     let table = document.createElement("table");
     table.style.borderCollapse = "collapse";
-    for (let row of data) {
-        console.log(row.a, row.b);
-        
+    for (let row of data)
+    {
+         console.log(row.a, row.b);
+         
+    for(let cell of row.cells) 
+    {
+       console.log(row.innerText)
     }
 }
+}*/
 
-
-/*let main = document.getElementById("main");
-const size = 2 ;
-let table = document.createElement("table");
-table.style.borderCollapse = "collapse";
-for (let y = 0; y < size; y++) {
-  let tr = document.createElement("tr");
-  for (let x = 0; x < size; x++) {
-    let td = document.createElement("td");
+function updateTable() {
+  form.onsubmit = (e) => {
+    e.preventDefault();
+  var values = {
+    age: document.getElementById("age").value,
+    book: document.getElementById("book").value,
    
-    td.innerText(age);
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
+  };
+
+  var table = document.getElementById("table");
+
+  Object.keys(values).forEach(function (key) {
+    table.getElementsByClassName(key + "Row")[0]
+      .getElementsByClassName("value")[0]
+      .textContent = values[key];
+  });
 }
-main.appendChild(table);*/
+}
+document.getElementById("button").addEventListener("click", updateTable);
+/*function addRow()
+            {
+                
+                var age = document.getElementById('age').value;
+                var book = document.getElementById('book').value;
+                
+                  
+            
+                  var table = document.getElementsByTagName('table')[0];
+                  
+                  var newRow = table.insertRow(table.rows.length/2+1);
+                  
+                
+                  var cel1 = newRow.insertCell(0);
+                  var cel2 = newRow.insertCell(1);
+          
+                  cel1.innerHTML = age;
+                  cel2.innerHTML = book;
+                  
+            }
+document.getElementById("button").addEventListener("click", addRow);*/
